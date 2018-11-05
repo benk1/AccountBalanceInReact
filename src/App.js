e75ec7e24cb2d5ffb739a5843c6381cbbf79a2f1
benk1 <kakengi_bernard@hotmail.com>
2018-11-05 14:45:08 +0200

    add clear function

--- src/App.js
+++ src/App.js
@@ -14,33 +14,35 @@ class App extends Component {
     {description: 'travel', amount: 1000}],
 
     description: "",
-
     amount: "",
-    transaction: "Income"
-   
+    transaction: "Income" 
   };
 
   handleChange = (e) => {
     this.setState({ 
     [e.target.name]: e.target.value
-    
     });
   }
-
+ 
+  clearFields = () => {
+    this.setState({
+      amount: "",
+      description: ""
+    })
+  }
 getadd = () => {
-  
+  this.clearFields();
   if(this.state.transaction === "Expense"){
     this.addExpense(this.state.description, this.state.amount);
   }else if((this.state.transaction === "Income")){
     this.addIncome(this.state.description, this.state.amount);
   }
-  this.refs.desc.value = "";
-  this.refs.amou.value = "";
+  
   
   }
-
+  
   addIncome = (description,amount) => {
-    
+ 
     this.setState({
     incomes: [...this.state.incomes,{description: description, amount: Number(amount)}]
   });
@@ -58,7 +60,7 @@ render() {
   
   const {description, amount} = this.state;
     
-    const incomeresult = this.state.incomes.map((income,i) => {
+  const incomeresult = this.state.incomes.map((income,i) => {
       
        return <li className="align" key={i}>{income.description} {income.amount} </li>
     });
@@ -71,9 +73,9 @@ render() {
       return <li className="align" key={i}>{expense.description} {expense.amount}</li>
    });
 
-   const totalexpenses = this.state.expenses.reduce((sum, i) => (sum +=  i.amount), 0)
+   const totalexpense = this.state.expenses.reduce((sum, i) => (sum +=  i.amount), 0)
 
-   const balance = totalincome - totalexpenses;
+   const balance = totalincome - totalexpense;
      
    
 return (
@@ -81,7 +83,7 @@ return (
     <Header title="Account Balance" />
   <div className="container">
       <input name="description" value={description} type="text" ref="desc"  placeholder="Enter description" onChange={this.handleChange}/>
-
+ 
       <input name="amount" value={amount} type="number" ref="amou"  placeholder="Enter the Amount" onChange={this.handleChange}/>
 
       <select name="transaction" value={this.state.value} onChange={this.handleChange}>
@@ -107,7 +109,7 @@ return (
          <p id="expenseParagraph">Expenses</p>
          <hr color="green" width="90%" align="CENTER" id="textExpense"></hr>
          {expenseresult}
-         <h3>Totalexpense: {totalexpenses }</h3>  
+         <h3>Totalexpense: {totalexpense }</h3>  
       </div> 
 
     </div>
